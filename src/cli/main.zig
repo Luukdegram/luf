deeeb1f43adfe084eafdcae6f6232e9a96a12fb0
Luuk de Gram <Luukdegram@users.noreply.github.com>
2020-09-25 20:20:40 +0200

    Allow compilation to bytecode and run bytecode directly using cli

--- src/cli/main.zig
+++ src/cli/main.zig
@@ -20,29 +20,19 @@ pub fn main() !void {
 
     const file_path = if (args.len >= 3) args[2] else return log.err("Missing file path: 'luf {} <file_path>'\n", .{action});
 
-    const file_type: enum { source, byte_code } = if (std.mem.endsWith(u8, file_path, ".luf"))
-        .source
-    else if (std.mem.endsWith(u8, file_path, ".blf"))
-        .byte_code
-    else
-        return log.err("Unsupported file type, expected a '.luf' or '.blf' file\n", .{});
-
-    const file = std.fs.cwd().openFile(file_path, .{}) catch |err| {
-        return log.err("Could not open file: '{}'\nError: {}\n", .{ file_path, err });
-    };
-
-    const file_data = try file.readToEndAlloc(gpa, std.math.maxInt(u64));
-    defer gpa.free(file_data);
-
-    var vm = try luf.Vm.init(gpa);
-    defer vm.deinit();
-
-    if (file_type == .source) {
-        vm.compileAndRun(file_data) catch |err| {
-            try vm.errors.write(file_data, std.io.getStdErr().writer());
-            return;
-        };
+    if (std.mem.eql(u8, action, "run")) {
+        try runner(gpa, file_path);
+        return;
     }
+
+    var i: usize = 0;
+    const output_name = while (i < args.len) : (i += 1) {
+        if (std.mem.eql(u8, args[i], "-o") and args.len > i + 1) {
+            break args[i + 1];
+        }
+    } else null;
+
+    try builder(gpa, file_path, output_name);
 }
 
 /// Parses the process' arguments
@@ -56,4 +46,81 @@ fn parse(gpa: *std.mem.Allocator) ![][]const u8 {
     }
 
     return list.toOwnedSlice();
+}
+
+/// Runs either a Luf source file or bytecode directly
+fn runner(gpa: *std.mem.Allocator, file_path: []const u8) !void {
+    const file_type: enum { source, byte_code } = if (std.mem.endsWith(u8, file_path, ".luf"))
+        .source
+    else if (std.mem.endsWith(u8, file_path, ".blf"))
+        .byte_code
+    else
+        return log.err("Unsupported file type, expected a '.luf' or '.blf' file\n", .{});
+
+    const file = std.fs.cwd().openFile(file_path, .{}) catch |err| {
+        return log.err("Could not open file: '{}'\nError: {}\n", .{ file_path, @errorName(err) });
+    };
+
+    var vm = try luf.Vm.init(gpa);
+    defer vm.deinit();
+
+    if (file_type == .source) {
+        const file_data = try file.readToEndAlloc(gpa, std.math.maxInt(u64));
+        defer gpa.free(file_data);
+
+        vm.compileAndRun(file_data) catch |err| {
+            try vm.errors.write(file_data, std.io.getStdErr().writer());
+        };
+        return;
+    }
+
+    var byte_code = luf.byte_code.ByteCode.decodeFromStream(gpa, file.reader()) catch |err| {
+        log.err("Could not decode bytecode: {}\n", .{@errorName(err)});
+        return err;
+    };
+    defer byte_code.deinit();
+
+    vm.loadCode(&byte_code);
+    vm.run() catch {
+        try vm.errors.write("", std.io.getStdErr().writer());
+    };
+}
+
+/// Compiles the luf source code and outputs its bytecode which can then be ran later
+fn builder(gpa: *std.mem.Allocator, file_path: []const u8, output_name: ?[]const u8) !void {
+    if (!std.mem.endsWith(u8, file_path, ".luf"))
+        return log.err("Expected file with luf extension: '.luf' in path '{}'\n", .{file_path});
+
+    const file = std.fs.cwd().openFile(file_path, .{}) catch |err| {
+        return log.err("Could not open file with path '{}'\nError recieved: {}\n", .{ file_path, err });
+    };
+
+    const source = try file.readToEndAlloc(gpa, std.math.maxInt(u64));
+    defer gpa.free(source);
+
+    var errors = luf.Errors.init(gpa);
+    defer errors.deinit();
+
+    var byte_code = luf.compiler.compile(gpa, source, &errors) catch |err| {
+        return errors.write(source, std.io.getStdErr().writer());
+    };
+    defer byte_code.deinit();
+
+    const final_output_name = output_name orelse blk: {
+        const base = std.fs.path.basename(file_path);
+        var new_name = try gpa.alloc(u8, base.len);
+        std.mem.copy(u8, new_name, base);
+        std.mem.copy(u8, new_name[new_name.len - 3 .. new_name.len], "blf");
+        break :blk new_name;
+    };
+    // if `output_name` was null, we allocated memory to create the new name
+    defer if (output_name == null) gpa.free(final_output_name);
+
+    const output_file = std.fs.cwd().createFile(final_output_name, .{}) catch |err| {
+        return log.err("Could not create output file '{}'\nError '{}'\n", .{ final_output_name, err });
+    };
+
+    byte_code.encodeToStream(output_file.writer()) catch |err| {
+        return log.err("Could not write to output file '{}'\nError '{}'\n", .{ final_output_name, err });
+    };
 }
--- src/luf.zig
+++ src/luf.zig
@@ -1,6 +1,7 @@
-pub const Lexer = @import("lexer.zig").Lexer;
-pub const Token = @import("token.zig").Token;
 pub const Vm = @import("vm.zig").Vm;
+pub const byte_code = @import("bytecode.zig");
+pub const compiler = @import("compiler.zig");
+pub const Errors = @import("error.zig").Errors;
 
 test "All tests" {
     _ = @import("vm.zig");
